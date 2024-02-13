d793a241e4eeefe631c21f57110a906bbf2d18f2
to0ony <antonijomaric1@gmail.com>
2024-02-13 13:09:30 +0100

    added paging, sorting, carfilter

---
+++ MonoCetvrtiDan/CarRent/CarRent.Common/Paging.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent.Common
+{
+    public class Paging
+    {
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 10;
+    }
+}
